8ab733da86000db2594b3ca7d286d1cc337ee118
DuarteB <eduarte.dev@gmail.com>
2022-05-22 12:06:19 -0300

    Printing data from an API with TypeScript

---
+++ logdata/index.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+interface Post {
+    userId: number;
+    title: string;
+    body: string;
+}
+
+const url = 'https://jsonplaceholder.typicode.com/posts/1';
+
+const printPost = (userId: number, title: string, body: string) => {
+    console.log(`
+            User ID is ${userId}
+            Title of post: ${title}
+            Post content: ${body}
+        `)
+}
+
+axios
+    .get(url)
+    .then(res => {
+        const post = res.data as Post;
+
+        const userId = post.userId;
+        const title = post.title;
+        const body = post.body;
+
+        printPost(userId, title, body)
+    })
+
+
+// {
+//     "userId": 1,
+//     "id": 1,
+//     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+// }
