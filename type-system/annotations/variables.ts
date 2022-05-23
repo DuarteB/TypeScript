c95ea13ad58b9df4f15c98d32b75a242d027acaf
DuarteB <eduarte.dev@gmail.com>
2022-05-22 21:24:56 -0300

    Starting with Annotations

---
+++ type-system/annotations/variables.ts
@@ -0,0 +1,23 @@
+let age: number = 20;
+
+let firstName: string = 'Eduardo';
+
+let  isMarried: boolean = false;
+
+let noValue: null = null;
+
+let noAssigned: undefined = undefined;
+
+let today: Date = new Date();
+
+//***************//
+
+let planets: string[] = [ 'Earth', 'Mars', 'Saturn', 'Jupiter' ];
+
+let luckyNumbers: number[] = [ 3, 5, 7 ];
+
+let completed: boolean[] = [ true, false, false, true ]
+
+//Classes
+class Phone{}
+let phone: Phone = new Phone();
